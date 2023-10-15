b216ab2ed2ace1627a4cf141db3268ffa13a6d86
Hoqueindadul <skindadul4321@gmail.com>
2023-10-15 09:53:56 +0530

    create delet, insert function

--- Array/array_creat_traverse.c
+++ Array/array_creat_traverse.c
@@ -1,31 +1,88 @@
+// program for create, insert, delete from an array.
+
 #include<stdio.h>
 
-void creat_array(int arr[], int s);
-void traverse_array(int arr[], int s);
+
+void creat_trvers(int ar[], int siz);
+void insert(int ar[], int siz, int elmt, int posi);
+void delet(int ar[], int siz, int posi);
 
 int main()
 {
-    int arr[50], size, res;
-    printf("Enter How Many Elements You Want: \n");
+    int arr[30], size, pos, elm;
+    int i;
+    printf("Enter how many elements you want\n");
     scanf("%d", &size);
-    creat_array(arr, size);
-    printf("After Traversing The array:\n");
-    traverse_array(arr, size);
+    printf("Enter your array elements: \n");
+    creat_trvers(arr, size);
+    printf("Enter your position:\n");
+    scanf("%d", &pos);
+    printf("enter your element: \n");
+    scanf("%d", &elm);
+    insert(arr, size, elm, pos);
+    printf("For delete\n");
+    printf("Enter your position to be delet: \n");
+    scanf("%d", &pos);
+    delet(arr, size, pos);
     
+    
+    return 0;
 }
+void creat_trvers(int ar[], int siz)
+{
+    int i;
+    for(i=0;i<siz;i++)
+    {
+        scanf("%d", &ar[i]);
+    }
+    printf("Before inserting more elements: \n");
+    for(i=0;i<siz;i++)
+    {
+        printf("%d ", ar[i]);
+    }
+}
+void insert(int ar[], int siz, int elmt, int posi)
+{
+    int ruf = siz, i;
+    while(ruf>=posi)
+    {
+        ar[ruf+1] = ar[ruf];
+        ruf --;
+    }
+    ar[posi] = elmt;
+    siz += 1;
+    printf("After inserting more elements: \n");
+    for(i=0;i<siz;i++)
+    {
+        printf("%d ", ar[i]);
+    }
+}
+void delet(int ar[], int siz, int posi)
+{
 
-void creat_array(int arr[], int s)
-{
-    printf("Enter your Array Elements: \n");
-    for(int i=0;i<s;i++)
+    int delelm, temp = posi;
+    printf("Enter your element to be delete: \n");
+    scanf("%d", &delelm);
+    if(delelm == ar[posi])
     {
-        scanf("%d", &arr[i]);
+        printf("%d is deleted", ar[posi]);
+        while(temp<=siz-1)
+        {
+            ar[temp] = ar[temp+1];
+            temp++;
+        }
+        siz = siz-1;
+        printf("After deleting elements: \n");
+        for(int i=0;i<=siz;i++)
+        {
+            printf("%d ", ar[i]);
+        }
     }
-}
-void traverse_array(int arr[], int s)
-{
-    for(int i = 0;i < s;i++)
+    else
     {
-        printf("%d ", arr[i]);
+        printf("Your entered number is not present in that position\n");
     }
+    
+    
+    
 }
